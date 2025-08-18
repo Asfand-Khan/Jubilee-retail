8c2bb0371f68ef866e07b9648701f7400976657c
Asfand-Khan <khanasfand174@gmail.com>
2025-08-18 16:40:41 +0500

    change authentication from password and secret to jwt auth

--- src/routes/motorQuoteRoutes.ts
+++ src/routes/motorQuoteRoutes.ts
@@ -1,12 +1,20 @@
 import { Router } from "express";
-import { authenticateApiUser, checkUserRights } from "../middleware/authMiddleware";
-import { createMotorQuoteHandler, getAllMotorQuotesHandler, getSingleMotorQuoteHandler, updateMotorQuoteHandler } from "../controllers/motorQuoteController";
+import {
+  authenticateApiUser,
+  authenticate,
+} from "../middleware/authMiddleware";
+import {
+  createMotorQuoteHandler,
+  getAllMotorQuotesHandler,
+  getSingleMotorQuoteHandler,
+  updateMotorQuoteHandler,
+} from "../controllers/motorQuoteController";
 
 const router = Router();
 
-router.get("/", authenticateApiUser, getAllMotorQuotesHandler); // Get All Motor Quotes --> Protected
+router.get("/", authenticate, getAllMotorQuotesHandler); // Get All Motor Quotes --> Protected
 router.post("/", authenticateApiUser, createMotorQuoteHandler); // Create Motor Quotes --> Protected
-router.get("/:id", authenticateApiUser, getSingleMotorQuoteHandler); // Single Motor Quotes --> Protected
-router.put("/", authenticateApiUser, updateMotorQuoteHandler); // Update Motor Quotes --> Protected
+router.get("/:id", authenticate, getSingleMotorQuoteHandler); // Single Motor Quotes --> Protected
+router.put("/", authenticate, updateMotorQuoteHandler); // Update Motor Quotes --> Protected
 
 export default router;
