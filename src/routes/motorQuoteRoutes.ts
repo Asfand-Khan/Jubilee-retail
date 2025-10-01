56460297b162d66daba25338235da50a9a0c37f7
Asfand-Khan <khanasfand174@gmail.com>
2025-10-01 12:19:20 +0500

    add motor quote status API

--- src/routes/motorQuoteRoutes.ts
+++ src/routes/motorQuoteRoutes.ts
@@ -8,13 +8,14 @@ import {
   getAllMotorQuotesHandler,
   getSingleMotorQuoteHandler,
   updateMotorQuoteHandler,
+  updateMotorQuoteStatusHandler,
 } from "../controllers/motorQuoteController";
 
 const router = Router();
 
 router.get("/", authenticate, getAllMotorQuotesHandler); // Get All Motor Quotes --> Protected
 router.post("/", authenticateApiUser, createMotorQuoteHandler); // Create Motor Quotes --> Protected
-router.post("/status", authenticate, createMotorQuoteHandler); // Update Motor Quote Status --> Protected
+router.post("/status", authenticate, updateMotorQuoteStatusHandler); // Update Motor Quote Status --> Protected
 router.get("/:id", authenticate, getSingleMotorQuoteHandler); // Single Motor Quotes --> Protected
 router.put("/", authenticate, updateMotorQuoteHandler); // Update Motor Quotes --> Protected
 
