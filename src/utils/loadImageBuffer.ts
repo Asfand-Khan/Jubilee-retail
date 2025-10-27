830b7b274e905678e3be5a85febe3b6455b93a5f
Asfand-Khan <khanasfand174@gmail.com>
2025-10-27 14:23:45 +0500

    changes

--- src/utils/loadImageBuffer.ts
+++ src/utils/loadImageBuffer.ts
@@ -16,3 +16,15 @@ export async function loadImageBuffer(localPath: string, remoteUrl?: string) {
     return null;
   }
 }
+
+export async function loadImageBufferFromUrl(remoteUrl: string) {
+  try {
+    const response = await axios.get(remoteUrl!, {
+      responseType: "arraybuffer",
+    });
+    return Buffer.from(response.data);
+  } catch (err: any) {
+    console.error(`Error loading image (${remoteUrl}):`, err.message);
+    return null;
+  }
+}
